056dcaf7042a77977612d24a8a8f5371338f8af8
cycycyyy <jccsomera@iskolarngbayan.pup.edu.ph>
2024-02-26 16:55:46 +0800

    Fixed workstatus section overflow

--- src/components/WorkStatus.vue
+++ src/components/WorkStatus.vue
@@ -22,16 +22,16 @@ const WORK_STATUS_OBJ_TRUE = [
 </script>
 
 <template>
-    <div class="text-white bg-primary rounded-lg p-14 font-manrope mt-40 mb-11 lg:flex justify-between gap-5 items-center">
+    <div class="text-white bg-primary rounded-lg p-8 lg:p-14 font-manrope mt-40 mb-11 lg:flex justify-between gap-5 items-center">
         <div>
             <div class="flex flex-col gap-3 lg:max-w-xl">
-                <span class="font-bold font-leagueSpartan text-[40px] leading-tight">Currently, I am not actively
+                <span class="font-bold font-leagueSpartan text-xl lg:text-[40px] leading-tight">Currently, I am not actively
                     seeking employment opportunities.</span>
-                <span class="font-bold">However, if you have a project in mind where you believe my assistance could be
+                <span class="font-bold text-sm lg:text-base">However, if you have a project in mind where you believe my assistance could be
                     valuable.
                     Don't hesitate to reach out to me through these platforms.</span>
             </div>
-            <div class="flex gap-5 text-sm mt-7">
+            <div class="flex gap-5 text-xs mt-7 flex-wrap">
                 <a href="" class="flex gap-2 items-center">
                     <Mail />Email
                 </a>
